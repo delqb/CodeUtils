85c1ee60ed9791e5bc27616f73ca636c6ed161be
Del <aelbanna3896@gmail.com>
2025-07-05 21:28:39 -0400

    Modify addlicense.py to accept a root directory path as an argument

--- addlicense.py
+++ addlicense.py
@@ -1,4 +1,6 @@
+import os
 from fileutility import traverse_directory, read_utf8_file, safe_replace_and_prepend_header
+import sys
 
 # Load headers
 previous_header_text = read_utf8_file("LICENSEHEADERPREV") or ""
@@ -10,8 +12,38 @@ def apply_latest_license_header(original_content, new_file, file_path, file_name
         original_content, new_file, file_path, new_header_text, previous_header_text)
 
 
-# Traverse and process files
-traverse_directory(
-    "./src",
-    apply_latest_license_header
-)
+def chooseDefault(dir_path):
+    while True:
+        choice = input(f"Default to '{dir_path}'? (Y/N) ").strip().lower()
+        if choice == "y":
+            return True
+        elif choice == "n":
+            return False
+        print("Invalid input. Please enter 'Y' or 'N'.")
+
+
+def main():
+    dir_path = os.path.join('.', 'src')
+
+    if len(sys.argv) > 1:
+        argument = sys.argv[1]
+        if os.path.isdir(argument):
+            dir_path = argument
+            print(f"Using '{argument}' as the target directory.")
+        else:
+            print(f"'{argument}' is not a valid directory.")
+            sys.exit(1)
+    else:
+        print(f"A target directory was not provided.")
+        if not chooseDefault(dir_path):
+            print(f"Exiting...")
+            sys.exit(1)
+
+    traverse_directory(
+        dir_path,
+        apply_latest_license_header
+    )
+
+
+if __name__ == "__main__":
+    main()
